3fac2bef02226875489ded974dd69ed3c910b48d
Michael Remediakis <m_remedi@berkeley.edu>
2018-06-07 20:13:15 -0700

    Added the ability to register as a mikey

--- mikeyblog.py
+++ mikeyblog.py
@@ -1,1 +1,9 @@
-from App import app
+from App import app, db
+from App.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+from werkzeug.security import generate_password_hash
